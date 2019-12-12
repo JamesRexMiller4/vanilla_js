2d03665a06f6defbd4334753a91432f61409155d
James Miller <james.rex.miller.4@gmail.com>
2019-12-12 07:57:59 -0700

    Add images and display on page, get fetch request working

---
+++ index.js
@@ -0,0 +1,7 @@
+// $('#stars-ideas-container')
+
+// (gatherAsteroids = () => {
+//   fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-12-18&end_date=2019-12-25&detailed=true&api_key=gftVqtJ1Aoj46G4XQO9Jq41fKMypL1PvCUFFfdEf')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// })();
